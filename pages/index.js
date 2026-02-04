7b4272193f1aee7a27b3a60d7ec2ddd1e5b6f431
Claricon Consult <claricon@Claricons-MacBook-Pro.local>
2026-02-04 18:32:20 +0000

    Configure Vercel for Nuxt

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+console.log("HOME PAGE LOADED");
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Circle</h1>
+      <p>Tap. Share. Customize your digital card.</p>
+
+      <p>
+        Please log in to customize your card and manage your static URL.
+      </p>
+    </div>
+  );
+}
